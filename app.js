e2e82c3b645cf5cb0d4d0b96f9bcac6b80431870
jamie0816 <jamie020816@gmail.com>
2015-12-02 15:23:00 -1000

    sd
    adsf

---
+++ app.js
@@ -0,0 +1,28 @@
+window.setTimeout("Tick()", 1000);
+function Tick() {
+}
+function UpdateTimer() {
+var Seconds = TotalSeconds;
+
+var Days = Math.floor(Seconds / 86400);
+Seconds -= Days * 86400;
+
+var Hours = Math.floor(Seconds / 3600);
+Seconds -= Hours * (3600);
+
+var Minutes = Math.floor(Seconds / 60);
+Seconds -= Minutes * (60);
+
+
+var TimeStr = ((Days > 0) ? Days + " days " : "") + LeadingZero(Hours) + ":" + LeadingZero(Minutes) + ":" + LeadingZero(Seconds)
+
+
+Timer.innerHTML = TimeStr;
+}
+
+
+function LeadingZero(Time) {
+
+return (Time < 10) ? "0" + Time : + Time;
+
+}
---
+++ index.html
@@ -0,0 +1,21 @@
+var Timer;
+var TotalSeconds;
+
+
+function CreateTimer(TimerID, Time) {
+Timer = document.getElementById(TimerID);
+TotalSeconds = Time;
+
+UpdateTimer()
+window.setTimeout("Tick()", 1000);
+}
+
+function Tick() {
+TotalSeconds -= 1;
+UpdateTimer()
+window.setTimeout("Tick()", 1000);
+}
+
+function UpdateTimer() {
+Timer.innerHTML = TotalSeconds;
+}
